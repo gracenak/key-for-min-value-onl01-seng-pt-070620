c4b0272b7dc2ba1a003adc42f335c216a47b3d8c
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-16 22:47:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_num == 0
-  min_name == 0
+  min_num = 0
+  min_name = 0
   name_hash.collect do |name, num|
-   if min_num == 0? name : min_name 
+   if min_num == 0 ? name : nil 
   end
-  nil
 end
